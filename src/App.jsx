c53037d90b58245343f35379adc68e4d68e4d1f1
unknown <iloriojomode@gmail.com>
2024-09-02 12:41:22 +0100

    yyeuj

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+export default function Foooter() {
+  return (
+    <div>
+      <div className="footer-area">
+        <div className="foot-1">
+          <div className="image-area">
+            <img src="/images/Frame 2.png" alt="" />
+            <h3>Shalimar Furniture Trading LLC</h3>
+          </div>
+          <div className="image-dept">
+            <div className="number">
+              <h2>
+                +971 6 5345837
+                <span className="empty"></span>
+                +971 50 4620553
+              </h2>
+            </div>
+            <div className="email">
+              <h2>Email:ritesh@shalimarcurtains.com</h2>
+              <p> </p>
+            </div>
+            <div className="email">
+              <h2>Website: www.shalimarcurtains.com</h2>
+              <p></p>
+            </div>
+            <div className="emaild">
+              <h2>
+                Address: Industrial Area 10, Sharjah <br />
+                United Arab Emirates
+              </h2>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div className="foot-2">
+          <div className="show">
+            <h2>
+              Riteshbr <br /> Jeswani
+            </h2>
+            <p>Managing Partner</p>
+          </div>
+        </div>
+      </div>
+      <div className="foot-part"></div>
+    </div>
+  );
+}
